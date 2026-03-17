0995bf65f11d7dc5cb956fad9f2aa0562d8eb00c
Jonnada Raju <22135A1204@gvpce.ac.in>
2026-03-17 20:15:49 +0530

    chore: initialize backend models package

---
+++ backend/models/__init__.py
@@ -0,0 +1,5 @@
+from backend.models.concept import SimulationDefinition, SimulationParameter
+from backend.models.interaction import SimulationRun
+from backend.models.subject import Subject
+
+__all__ = ['SimulationDefinition', 'SimulationParameter', 'SimulationRun', 'Subject']
